d5af59d797818f0a6b0a42222c754cf8cfa9010a
wolfsgrin8d <wolfsgrin8d@gmail.com>
2021-11-16 21:31:07 +0200

    2nd commit. Changes were implemented.

--- src/Dog.java
+++ src/Dog.java
@@ -1,87 +1,99 @@
+
+import java.lang.constant.Constable;
 import java.sql.SQLOutput;
 import java.util.Scanner;
-public class Dog {
-    public static void main(String[] args) {
-        Doggie doggie1 = new Doggie();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please type your dog's name, this is a required parameter");
-        String doggieName = scanner.next();
-        doggie1.setName(doggieName);
-        doggie1.getInfo();
-        doggie1.feedDog();
-        doggie1.walkDog();
-        doggie1.playDog();
-    }
 
-}
 class Doggie {
-    String name;
+    private String name;
     int bellyful;
     int walking;
     int games;
-    public Doggie(/*String username*/){
-        /*this.name = username;*/
-        this.bellyful = 1 +(int)(Math.random() * 100) ;
-        this.walking = 1 +(int)(Math.random() * 100) ;
-        this.games = 1 +(int)(Math.random() * 100) ;
+    boolean feed;
+    boolean walk;
+    boolean game;
+    private int step1 = 5;
+    private int step2 = 10;
+    private int step3 = 13;
+
+    public Doggie(String username) {
+        this.name = username;
+        this.bellyful = 1 + (int) (Math.random() * 100);
+        this.walking = 1 + (int) (Math.random() * 100);
+        this.games = 1 + (int) (Math.random() * 100);
     }
+
     public void setName(String username) {
-        if(username.isEmpty()){
+        this.name = username;
+        while (name.isEmpty()) {
             System.out.println("Name cannot be empty!");
+            Scanner again = new Scanner(System.in);
+            this.name = again.nextLine();
+        }
+    }
+
+    public void getInfo() {
+        System.out.println(name + ", bellyfull = " + bellyful + ", walking = " + walking + ", games = " + games);
+    }
+
+    public void healthcheck() {
+        if (bellyful < 60) {
+            System.out.println("Your dog is hungry! Do you wanna feed it? Type true or false.");
+            Scanner scan = new Scanner(System.in);
+            feed = scan.nextBoolean();
         } else {
-            this.name = username;}}
-
-    public void getInfo(){
-        System.out.println(name +", bellyfull = "+bellyful+", walking = "+walking+", games = "+games);}
-
-        int returnBellyFul(){
-            return this.bellyful;
+            System.out.println("Your dog isn't hungry.");
+            feed = false;
         }
-        public void feedDog(){
-            int a = this.bellyful;
-            if (a<60){
-                for(int i = a;  i < 100;i = i+5){
-                    if(i > 95){
-                        System.out.println("bellyful is "+i);
-                        break;}
-                    else{
-                    System.out.println("bellyful is "+i);}
-                }
-            } else{
-                System.out.println("Your dog was already full.");
-            }
-
-    }
-            public void walkDog(){
-                int b = this.walking;
-                if (b<50){
-                    for(int x = b;  x < 100;x = x+10){
-                        if( x > 90){
-                            System.out.println("walking is "+x);
-                            break;}
-                        else{
-                            System.out.println("walking is "+x);}
-                    }
-                } else{
-                    System.out.println("Your dog was already walked.");
-                }
-
+        if (walking < 50) {
+            System.out.println("Your dog wants to go outside. I'll walk your dog.");
+            walk = true;
+        } else {
+            System.out.println("Your dog doesn't want to go outside.");
+            walk = false;
         }
-    public void playDog(){
-        int c = this.games;
-        if (c<40){
-            for(int y = c;  y < 100;y = y+10){
-                if( y > 87){
-                    System.out.println("playing is "+y);
-                    break;}
-                else{
-                    System.out.println("playing is "+y);}
-            }
-        } else{
-            System.out.println("Your dog was already played with.");
+        if (games < 40) {
+            System.out.println("Your dog wants to play. I'll play with your dog.");
+            game = true;
+        } else {
+            System.out.println("Your dog doesn't want to play.");
+            game = false;
         }
-
     }
 
+    public void feedDog() { // разбить на два метода, хелс чек и кормить
+        if (feed) {
+            while (bellyful < 96) {
+                bellyful = bellyful + step1;
+                System.out.println("bellyful is " + bellyful + " now.");
+            }
+        } else {
+            System.out.println("Skip feeding.");
+        }
+    }
 
+    public void walkDog() {
+        if (walk) {
+            while (walking < 91) {
+                walking = walking + step2;
+                System.out.println("walking is " + walking + " now.");
+            }
+        } else {
+            System.out.println("Skip walking.");
+        }
+    }
+
+    public void playDog() {
+        if (game) {
+            while (games < 88) {
+                games = games + step3;
+                System.out.println("games is " + games + " now.");
+            }
+        } else {
+            System.out.println("Skip playing.");
+        }
+    }
 }
+
+
+
+
